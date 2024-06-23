97acd8bc455ebb8ab72f974db08501d19bcb267b
Taras Lysenko <tlisenko@phasev.com>
2024-06-24 01:16:29 +0200

    init commit

---
+++ todolist.client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import TodoList from './components/TodoList';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Todo App</h1>
+            <TodoList />
+        </div>
+    );
+};
+
+export default App;
---
+++ todolist.client/src/services/api.js
@@ -0,0 +1,32 @@
+const API_URL = '/api/todo';
+
+const getTodos = async () => {
+    const response = await fetch(API_URL);
+    return response.json();
+};
+
+const addTodo = async (todo) => {
+    const response = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(todo),
+    });
+    return response.json();
+};
+
+const markComplete = async (id) => {
+    const response = await fetch(`${API_URL}/${id}/complete`, {
+        method: 'POST',
+    });
+    if (!response.ok) {
+        throw new Error('Failed to mark complete');
+    }
+};
+
+export default {
+    getTodos,
+    addTodo,
+    markComplete
+};
